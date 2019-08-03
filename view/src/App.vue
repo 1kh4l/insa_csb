e5db949e3e76be1c2d025df85c155402ea8eabf0
1kh4l <jhon.pantoja@correounivalle.edu.co>
2019-08-03 00:41:12 -0500

    Creating INSA navigation bar

--- view/src/App.vue
+++ view/src/App.vue
@@ -1,36 +1,29 @@
 <template>
-  <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>INSA</span>
-        <span class="font-weight-light"> - Instituto Nuestra Señora de la Asunción</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <HelloWorld msg="Welcome to the new INSA's web site"/>
-    </v-content>
-  </v-app>
+    <v-app>
+        <v-app-bar app>
+            <InsaNavBar/>
+        </v-app-bar>
+        <v-content>
+            <HelloWorld msg="Welcome to the new INSA's web site"/>
+        </v-content>
+    </v-app>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue';
+import InsaNavBar from './components/InsaNavBar.vue';
 
 export default {
     name: 'App',
     components: {
         HelloWorld,
+        InsaNavBar,
     },
     data: () => ({
         //
     }),
 };
 </script>
+
+<style>
+</style>
