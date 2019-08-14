a6c0e9429a84b40737529e26acef83a6476a0fdb
1kh4l <jhon.pantoja@correounivalle.edu.co>
2019-08-13 23:37:07 -0500

    Creating another route for Basilians component

--- view/src/router/index.js
+++ view/src/router/index.js
@@ -1,4 +1,5 @@
 import InsaHome from '@/components/InsaHome.vue';
+import Basilians from '@/components/Basilians.vue';
 
 export default [
     {
@@ -8,5 +9,9 @@ export default [
     {
         path: '/home',
         component: InsaHome,
+    },
+    {
+        path: '/basilians',
+        component: Basilians,
     },
 ];
