b4f106ff8c4c8bc056642591191c82c2b2d61ccf
1kh4l <jhon.pantoja@correounivalle.edu.co>
2019-09-25 21:17:23 -0500

    Isolating entire ui repo

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import FlagIcon from 'vue-flag-icon';
+import VueRouter from 'vue-router';
+import Vue from 'vue';
+import App from './App';
+import store from './store';
+import vuetify from './plugins/vuetify';
+import i18n from './plugins/i18n';
+import Router from './router';
+
+Vue.use(VueRouter);
+Vue.use(FlagIcon);
+
+Vue.config.productionTip = false;
+
+/* Router */
+const router = new VueRouter({
+    routes: Router,
+});
+
+new Vue({
+    vuetify,
+    render: h => h(App),
+    store,
+    FlagIcon,
+    i18n,
+    router,
+}).$mount('#app');
