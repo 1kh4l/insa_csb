3706b71c85552279cc1c68be026339eb43639d5b
agderes <esra.agdere@gmail.com>
2020-04-28 16:42:31 +0300

    Add INSA admission/how to apply

--- src/components/InsaAdmission.vue
+++ src/components/InsaAdmission.vue
@@ -4,37 +4,33 @@
       <v-row align="center" justify="center">
         <v-col class="dense">
           <div>
-            <span class="brown--text display-2">
-              {{ $t("content-insa-admission.requirements[0]") }}
-            </span>
-            <p class="text-justify">
-              {{ $t("content-insa-admission.requirements[1]") }}
-            </p>
-            <p class="text-justify">
-              {{ $t("content-insa-admission.requirements[2]") }}
-            </p>
-            <p class="text-justify">
-              {{ $t("content-insa-admission.requirements[3]") }}
-            </p>
-          </div>
-          <div class="insa-admission" id="insa-admission">
-            <span class="brown--text display-2">
-              {{ $t("content-insa-admission.how-to-apply[0]") }}
-            </span>
-            <p class="text-justify">
-              {{ $t("content-insa-admission.how-to-apply[1]") }}
-            </p>
+            <v-card flat class="requirements">
+              <v-card-title class="brown--text display-2">
+                {{ $t("content-insa-admission.requirements[0]") }}
+              </v-card-title>
+              <v-card-text class="body-1 font-weight-black">
+                <p class="text-justify">{{ $t("content-insa-admission.requirements[1]") }}</p>
+                <p class="text-justify">{{ $t("content-insa-admission.requirements[2]") }}</p>
+                <p class="text-justify">{{ $t("content-insa-admission.requirements[3]") }}</p>
+              </v-card-text>
+              <v-card-title class="brown--text display-2">
+                {{ $t("content-insa-admission.how-to-apply[0]") }}
+              </v-card-title>
+              <v-card-text class="body-1 font-weight-black">
+                <p class="text-justify">{{ $t("content-insa-admission.how-to-apply[1]") }}</p>
+              </v-card-text>
+            </v-card>
           </div>
           <v-row v-for="step in stepsAdmission" :key="step">
             <v-col>
-              <v-card max-width="500" color="orange lighten-5" shaped>
-                <v-row>
-                  <v-avatar color="orange" class="ma-3" size="90" title>
+              <v-card max-width="500" color="orange lighten-5" justify="center" shaped>
+                <v-row align="space-around">
+                  <v-avatar color="orange" class="ma-5" size="90" title>
                     <span class="white--text font-weight-medium">
                       {{ $t(`content-insa-admission.step-${step}[0]`) }}
                     </span>
                   </v-avatar>
-                  <v-card-text>
+                  <v-card-text class="body-1 font-weight-medium">
                     <div class="admission-step">
                       {{ $t(`content-insa-admission.step-${step}[1]`) }}
                     </div>
@@ -63,8 +59,12 @@ export default {
   position: relative;
   display: grid;
   grid-template-rows: 1fr;
+  max-width: 340px;
 }
 .v-card__text {
   width: unset;
+}
+.container {
+  max-width: 1000px;
 }
 </style>
