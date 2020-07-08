fa745eee1cba55d1bde3497ab4ee20fff1652e17
1kh4l <jhon.pantoja@correounivalle.edu.co>
2020-07-08 23:00:19 +0200

    Add svg imports library

--- vue.config.js
+++ vue.config.js
@@ -9,6 +9,14 @@ module.exports = {
     },
   },
   chainWebpack: (config) => {
+    const svgRule = config.module.rule('svg');
+    svgRule.uses.clear();
+    svgRule
+          .use('babel-loader')
+          .loader('babel-loader')
+          .end()
+          .use('vue-svg-loader')
+          .loader('vue-svg-loader');
     config.module.rule('ts');
     config.module.rule('ts').use('ts-loader');
     config.module.rule('ts').use('babel-loader');
